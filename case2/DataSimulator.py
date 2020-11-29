af70f2bbd0e3fb0860dbbffcf007ce0baabdd6e8
sidazhong <sidazhong>
2020-11-29 14:53:45 -0800

    sida

--- case2/DataSimulator.py
+++ case2/DataSimulator.py
@@ -5,12 +5,6 @@ import os
 import ECC
 
 
-def getKeyAndSignature(input_text):
-    G = ECC.Point(397, -2, 2, (284, 118))
-    ecc = ECC.SimpleECC(G)
-    signature = ecc.sign(input_text)
-    return {"G": G, "pk":ecc.key(), "msg":input_text, "signature": signature}
-
 
 #RSAkey.verify(hash,      signature)    # This sig will check out
 #RSAkey.verify(hash[:-1], signature)    # This sig will fail
@@ -23,7 +17,7 @@ class DataSimulator:
     def __init__(self):
         self.timer = 0
 
-        raw_data = open(os.path.dirname(__file__)+"/headlinesNew.json").read()
+        raw_data = open(os.path.dirname(__file__)+"/transaction.json").read()
 
         self.headlines = json.loads(raw_data)
         self.keys = self.headlines.keys()
@@ -33,3 +27,13 @@ class DataSimulator:
         key = list(self.keys)[self.timer]
         self.timer = min(self.timer+1, len(self.keys)-1)
         return self.headlines[key]
+
+    def getKeyAndSignature(self,input_text):
+        G = ECC.Point(463, -2, 2, (155, 452))
+        ecc = ECC.SimpleECC(G)
+        signature = ecc.sign(input_text)
+        pk = ecc.exportKey()
+
+        pk=pk.replace("Curve( 463 -2 2 ); G( 155 452 ); PK( ", "")
+        pk=pk.replace(" ); PKOrder( 149 )", "")
+        return {"pk":pk, "signature": [signature[0],signature[1]]}
